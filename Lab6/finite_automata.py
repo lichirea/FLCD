32ecbab03a6fe66884691653ba2a591a508fc70b
Liviu Chirea <li.chirea28@gmail.com>
2022-11-23 09:38:44 +0200

    finished and presented finite automata

--- Lab6/finite_automata.py
+++ Lab6/finite_automata.py
@@ -14,10 +14,41 @@ class FiniteAutomata:
         states = file[0].split(' ')
         for state in states:
             self.states.append(state)
-        print(self.states)
 
         alphabet = file[1].split(' ')
         for letter in alphabet:
-            if letter !=
             self.alphabet.append(letter)
-        print(self.alphabet)
+
+        self.initial_state = file[2]
+
+        final_states = file[3].split(' ')
+        for state in final_states:
+            self.final_states.append(state)
+
+        deterministic = True
+        for i in range(4, len(file)):
+            line = file[i].split(' ')
+            if self.transitions.get((line[0], line[1])):
+                self.transitions[(line[0], line[1])].append(line[2])
+                deterministic = False
+            else:
+                self.transitions[(line[0], line[1])] = [line[2]]
+
+        if deterministic:
+            print("The FA is a DFA!!!")
+        else:
+            print("THE FA is NOT a DFA")
+
+    def check_seq(self, seq):
+        state = self.initial_state
+        next_state = None
+        for s in seq:
+            if self.transitions.get((state, s)):
+                next_state = self.transitions.get((state, s))[0]
+            if next_state != None:
+                state = next_state
+                next_state = None
+        if state in self.final_states:
+            print("ACCEPTED")
+        else:
+            print("NOT ACCEPTED")
--- Lab6/main.py
+++ Lab6/main.py
@@ -2,14 +2,26 @@ from finite_automata import FiniteAutomata
 
 
 def main():
-    fa = FiniteAutomata("Lab4/FA.in")
+    fa = FiniteAutomata("Lab6/FA.in")
 
     while True:
         print_menu()
-        choice = input("Choose:")
+        choice = input("\nPlease choose:")
         match choice:
             case "1":
-                pass
+                print(fa.states)
+            case "2":
+                print(fa.alphabet)
+            case "3":
+                print(fa.transitions)
+            case "4":
+                print(fa.initial_state)
+            case "5":
+                print(fa.final_states)
+            case "6":
+                seq = input("Enter a sequence:").strip()
+                seq = seq.split(' ')
+                fa.check_seq(seq)
             case _:
                 pass
 
@@ -20,6 +32,7 @@ def print_menu():
     print('3.Transitions')
     print('4.Initial State')
     print('5.Final States')
+    print('6.Enter and check sequence')
 
 
 if __name__ == "__main__":
